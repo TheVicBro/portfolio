b964e0d884a91b281f6e58ac86de33d87e1e8523
Victor Chung <dragonslayer3211@gmail.com>
2024-06-17 16:09:18 -0400

    animated buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,56 +18,87 @@ export default function Home() {
           <div>Contact</div>
         </div>
       </motion.div>
-      <div className="flex justify-between pt-48 px-48">
-        <div className="flex flex-col justify-between">
-          <div> 
-            <div className="truncate">
-              <motion.div 
-                className="mt-4 text-12xl font-bold text-lightblack leading-none"
-                initial={{ y: "100%" }}
-                animate={{ y: "0%" }}
-                transition={{ duration: 1, ease: "easeOut" }}
-              >
-                VICTOR CHUNG
-              </motion.div>
-            </div>
-            <div className="mt-2 px-12">
-              <div className="truncate pb-2">
+      <div className="h-screen sticky top-0">
+        <div className="flex justify-between pt-48 px-48">
+          <div className="flex flex-col justify-between">
+            <div> 
+              <div className="truncate">
                 <motion.div 
-                  className="text-7xl font-bold text-skyblue"
-                  initial={{ y: "-110%" }}
+                  className="mt-4 text-12xl font-bold text-lightblack leading-none"
+                  initial={{ y: "100%" }}
                   animate={{ y: "0%" }}
                   transition={{ duration: 1, ease: "easeOut" }}
                 >
-                  Full Stack Developer
+                  VICTOR CHUNG
                 </motion.div>
               </div>
-              <div className="mt-6 space-x-8">
-                <button className="rounded-lg bg-white text-3xl font-medium p-6 px-8">View Portfolio</button>
-                <button className="rounded-lg bg-white text-3xl font-medium p-6 px-8">View Resume</button>
+              <div className="mt-2 px-12">
+                <div className="truncate pb-2">
+                  <motion.div 
+                    className="text-7xl font-bold text-skyblue"
+                    initial={{ y: "-110%" }}
+                    animate={{ y: "0%" }}
+                    transition={{ duration: 1, ease: "easeOut" }}
+                  >
+                    Full Stack Developer
+                  </motion.div>
+                </div>
+                <div className="mt-6 space-x-8">
+                  <motion.button
+                    className="rounded-lg bg-white text-3xl font-medium py-6 overflow-hidden whitespace-nowrap"
+                    initial={{ width: 0, paddingLeft: 0, paddingRight: 0 }}
+                    animate={{ width: "18rem", paddingLeft: "2rem", paddingRight: "2rem" }}
+                    transition={{ duration: 1, ease: "easeOut" }}
+                  >
+                    <p className="line-clamp-1">View Portfolio</p>
+                  </motion.button>
+                  <motion.button
+                    className="rounded-lg bg-white text-3xl font-medium py-6 overflow-hidden whitespace-nowrap"
+                    initial={{ width: 0, paddingLeft: 0, paddingRight: 0 }}
+                    animate={{ width: "18rem", paddingLeft: "2rem", paddingRight: "2rem" }}
+                    transition={{ duration: 1, ease: "easeOut" }}
+                  >
+                    <p className="line-clamp-1">View Resume</p>
+                  </motion.button>
+                </div>
+              </div>
+            </div>
+            <div className="flex justify-between items-end">
+              <div className="flex space-x-4 px-12">
+                <motion.button 
+                  className="flex justify-center items-center rounded-lg bg-skyblue w-16 h-16"
+                  initial={{ width: 0 }}
+                  animate={{ width: "4rem" }}
+                  transition={{ duration: 1, ease: "easeOut" }}
+                  layout="preserve-aspect"
+                >
+                  <img src="/linkedin.png" className="w-14 h-14"/>
+                </motion.button>
+                <motion.button 
+                  className="flex justify-center items-center rounded-lg bg-skyblue w-16 h-16"
+                  initial={{ width: 0 }}
+                  animate={{ width: "4rem" }}
+                  transition={{ duration: 1, ease: "easeOut" }}
+                  layout="preserve-aspect"
+                >
+                  <img src="/github.png" className="h-16 w-16"/>
+                </motion.button>
+              </div>
+              <div className="relative">
+                <div className="absolute z-10 bottom-40 right-20 w-64 h-56 bg-white rounded-lg"></div>
+                <div className="absolute z-10 bottom-0 right-48 w-64 h-56 bg-white rounded-lg"></div>
               </div>
             </div>
           </div>
-          <div className="flex justify-between items-end">
-            <div className="flex space-x-4 px-12">
-              <button className="flex justify-center items-center rounded-lg bg-skyblue w-16 h-16">
-                <img src="/linkedin.png" className="w-14 h-14"/>
-              </button>
-              <button className="flex justify-center items-center rounded-lg bg-skyblue w-16 h-16">
-                <img src="/github.png" className="h-16 w-16"/>
-              </button>
-            </div>
-            <div className="relative">
-              <div className="absolute z-10 bottom-40 right-20 w-64 h-56 bg-white rounded-lg"></div>
-              <div className="absolute z-10 bottom-0 right-48 w-64 h-56 bg-white rounded-lg"></div>
+          <div className="bg-lightblack w-1/4 h-160 rounded-lg">
+            <div className="w-full h-full p-6">
+              <img src="/profilepic.jpg" alt="profile" className="rounded-lg w-full h-full object-cover" />
             </div>
           </div>
         </div>
-        <div className="bg-lightblack w-1/4 h-160 rounded-lg">
-          <div className="w-full h-full p-6">
-            <img src="/profilepic.jpg" alt="profile" className="rounded-lg w-full h-full object-cover" />
-          </div>
-        </div>
+      </div>
+      <div className="relative bg-lightblack z-20 h-screen rounded-3xl">
+        test
       </div>
     </main>
   );
